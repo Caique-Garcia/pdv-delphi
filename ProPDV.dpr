855284117489b86027521292bb3398b79a82c343
Caique Ferreira <111088915+Caique-Garcia@users.noreply.github.com>
2023-05-29 09:24:39 -0300

    Criando PDV com Delphi

---
+++ ProPDV.dpr
@@ -0,0 +1,21 @@
+program ProPDV;
+
+uses
+  Vcl.Forms,
+  ProjetoPDV.View.Principal in 'view\ProjetoPDV.View.Principal.pas' {formPrincipalPDV},
+  ProPDV.View.Login in 'view\ProPDV.View.Login.pas' {formLogin},
+  ProPDV.View.FinalizarVenda in 'view\ProPDV.View.FinalizarVenda.pas' {formFinalizarVenda},
+  ProPDV.View.FramePix in 'view\ProPDV.View.FramePix.pas' {framePix: TFrame},
+  ProPDV.View.FrameCartao in 'view\ProPDV.View.FrameCartao.pas' {FrameCartao: TFrame},
+  ProPDV.View.FrameDinheiro in 'view\ProPDV.View.FrameDinheiro.pas' {FrameDinheiro: TFrame},
+  ProPDV.View.IdentificarCliente in 'view\ProPDV.View.IdentificarCliente.pas' {FormIdentificarCliente};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TformLogin, formLogin);
+  Application.CreateForm(TformPrincipalPDV, formPrincipalPDV);
+  Application.Run;
+end.
